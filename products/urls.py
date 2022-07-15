e5c45aa0e687defd5a9823aa628198b819cf00c0
waiterorg <iwaiterorg@gmail.com>
2022-07-15 17:58:13 +0430

    drf setup

--- products/urls.py
+++ products/urls.py
@@ -21,4 +21,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls")),
+    path("api-auth/", include("rest_framework.urls")),
 ]
