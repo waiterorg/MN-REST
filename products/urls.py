d40ccdbdba68057bba2b0734ba2eb1846d79aa69
waiterorg <iwaiterorg@gmail.com>
2022-07-16 00:50:38 +0430

    home page in config urls

--- products/urls.py
+++ products/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from xml.etree.ElementInclude import include
 
+from core.views import HomeView
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core.urls")),
+    path("", HomeView.as_view()),
+    path("api/", include("core.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
