a384914000136dcebe9582be0ee5b124f2a87e15
waiterorg <iwaiterorg@gmail.com>
2022-07-15 17:20:47 +0430

    Create home page

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Product
+
+
+class HomeView(ListView):
+    template_name = "pages/home.html"
+
+    def get_queryset(self):
+        return Product.objects.filter_active_status()[:6]
