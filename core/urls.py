682ae9ecd3fa8c17d0fd16f10da89b5a55d6f204
waiterorg <iwaiterorg@gmail.com>
2022-07-15 19:40:52 +0430

    start with modelview set for mobile api

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,13 @@
-from django.contrib import admin
 from django.urls import path
+from rest_framework import routers
 
-from .views import HomeView
+from .views import HomeView, MobileViewSet
+
+router = routers.DefaultRouter()
+router.register("mobile", MobileViewSet, basename="mobiles")
 
 urlpatterns = [
     path("", HomeView.as_view()),
 ]
+
+urlpatterns += router.urls
--- core/views.py
+++ core/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
 from django.views.generic import ListView
+from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
-from .models import Product
+from .models import Mobile
+from .serializers import MobileSerializer
 
 
 class HomeView(ListView):
     template_name = "pages/home.html"
 
     def get_queryset(self):
-        return Product.objects.filter_active_status()[:6]
+        return Mobile.objects.filter_active_status()[:6]
+
+
+class MobileViewSet(ModelViewSet):
+    queryset = Mobile.objects.filter_active_status().prefetch_related(
+        "category"
+    )
+    serializer_class = MobileSerializer
+    filterset_fields = ["price", "production_date"]
+    ordering_fields = ["price", "title"]
+    search_fields = [
+        "title",
+    ]
+    ordering = ["-created"]
