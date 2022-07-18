b7fe62c1f2c964655df86ba2895eb130ac3510b4
waiterorg <iwaiterorg@gmail.com>
2022-07-18 05:09:38 +0430

    Add change password url

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenRefreshView
 
-from .views import EmailTokenObtainPairView, RegisterView
+from .views import ChangePasswordView, RegisterView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="token_obtain_pair"),
     path(
-        "token/obtain/",
-        EmailTokenObtainPairView.as_view(),
-        name="token_obtain_pair",
+        "change-password/",
+        ChangePasswordView.as_view(),
+        name="change-password",
     ),
-    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
