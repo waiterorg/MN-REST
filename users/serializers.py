1b3ac25c7d8644398234b8bd5b81a79221281754
waiterorg <iwaiterorg@gmail.com>
2022-07-18 05:09:24 +0430

    Add change password serializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,15 +1,23 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
-from rest_framework_simplejwt.serializers import (
-    TokenObtainPairSerializer as JwtTokenObtainPairSerializer,
-)
-
-
-class TokenObtainPairSerializer(JwtTokenObtainPairSerializer):
-    username_field = get_user_model().USERNAME_FIELD
 
 
 class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer for user endpoint .
+    """
+
     class Meta:
         model = get_user_model()
         fields = ("email", "username", "password")
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    """
+    Serializer for password change endpoint .
+    """
+
+    model = get_user_model()
+
+    old_password = serializers.CharField(required=True)
+    new_password = serializers.CharField(required=True)
