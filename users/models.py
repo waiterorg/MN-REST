57092cbf97645302d3a6818c37cd4439628bc675
waiterorg <iwaiterorg@gmail.com>
2022-07-18 05:06:28 +0430

    Delete custom user manager

--- users/models.py
+++ users/models.py
@@ -2,51 +2,28 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from phonenumber_field.modelfields import PhoneNumberField
 
 
-class CustomUserManager(BaseUserManager):
+class User(AbstractUser):
     """
-    Custom user model manager where email is the unique identifiers
-    for authentication instead of usernames.
+    Custom user model includes unique username, email and phone number .
     """
 
-    def create_user(self, email, password, **extra_fields):
-        """
-        Create and save a User with the given email and password.
-        """
-        if not email:
-            raise ValueError(_("The Email must be set"))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        """
-        Create and save a SuperUser with the given email and password.
-        """
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        extra_fields.setdefault("is_active", True)
-
-        if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superuser must have is_staff=True."))
-        if extra_fields.get("is_superuser") is not True:
-            raise ValueError(_("Superuser must have is_superuser=True."))
-        return self.create_user(email, password, **extra_fields)
-
-
-class CustomUser(AbstractUser):
     username = models.CharField(
         verbose_name=_("username"), max_length=255, unique=True
     )
     email = models.EmailField(verbose_name=_("email address"), unique=True)
+    phone = PhoneNumberField(
+        verbose_name=_("phone number"),
+        null=True,
+        blank=True,
+        max_length=20,
+        unique=True,
+    )
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["username"]
-
-    objects = CustomUserManager()
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
 
     def __str__(self):
         return self.email
