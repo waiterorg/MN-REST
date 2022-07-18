6d89062bfe940dddc66fd41be3cacc823c9b829a
waiterorg <iwaiterorg@gmail.com>
2022-07-18 05:07:40 +0430

    authentication backend with username ,email and phone

--- users/auth_backends.py
+++ users/auth_backends.py
@@ -1,16 +1,19 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 
 
-class EmailBackend(ModelBackend):
-    def authenticate(self, request, **kwargs):
+class UsernameOrPhoneOrEmailBackend(ModelBackend):
+    def authenticate(self, request, username=None, password=None):
+        """
+        Custom authentication with username or email or phone number .
+        """
         UserModel = get_user_model()
         try:
-            email = kwargs.get("email", None)
-            if email is None:
-                email = kwargs.get("username", None)
-            user = UserModel.objects.get(email=email)
-            if user.check_password(kwargs.get("password", None)):
+            user = UserModel.objects.get(
+                Q(username=username) | Q(email=username) | Q(phone=username)
+            )
+            if user.check_password(password):
                 return user
         except UserModel.DoesNotExist:
             return None
